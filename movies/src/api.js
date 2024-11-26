7bfcf04fb41c8e6e719bf27e5224fbfc97f96401
Nishanth233 <nishanth10102323@gmail.com>
2024-11-26 14:57:08 +0530

    Movies_Search_App

--- movies/src/api.js
+++ movies/src/api.js
@@ -1,4 +1,4 @@
-const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+const API_KEY = "45259d3b";
 const BASE_URL = "http://www.omdbapi.com/";
 
 export const fetchMovies = async (query, page = 1, type = "") => {
