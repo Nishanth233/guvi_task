84570f35ae2293c8def6686bd361c9cb51179be1
Nishanth233 <nishanth10102323@gmail.com>
2024-12-29 12:10:24 +0530

    flight

--- flight-booking-system/backend/server.js
+++ flight-booking-system/backend/server.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const flightRoutes = require('./routes/flightRoutes');
-const bookingRoutes = require('./routes/bookingRoutes');
-const paymentRoutes = require('./routes/paymentRoutes');
-const notificationRoutes = require('./routes/notificationRoutes');
-const userRoutes = require('./routes/userRoutes');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const flightRoutes = require("./routes/flightRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
+const notificationRoutes = require("./routes/notificationRoutes");
+const userRoutes = require("./routes/userRoutes");
+
 dotenv.config();
 
 const app = express();
@@ -15,11 +16,15 @@ connectDB();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/api/users', userRoutes);
-app.use('/api/flights', flightRoutes);
-app.use('/api/bookings', bookingRoutes);
-app.use('/api/payments', paymentRoutes);
-app.use('/api/notifications', notificationRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/flights", flightRoutes);
+app.use("/api/bookings", bookingRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/notifications", notificationRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
